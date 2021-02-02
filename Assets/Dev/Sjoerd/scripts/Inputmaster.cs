33dd0298dcd6a1a50d52976002e3724cb814fc9e
SjoerdDeKoning <78423327+SjoerdDeKoning@users.noreply.github.com>
2021-02-02 21:46:43 +0100

    player_controler so far

--- Assets/Dev/Sjoerd/scripts/Inputmaster.cs
+++ Assets/Dev/Sjoerd/scripts/Inputmaster.cs
@@ -29,9 +29,9 @@ public class @Inputmaster : IInputActionCollection, IDisposable
             ],
             ""bindings"": [
                 {
-                    ""name"": ""1D Axis"",
-                    ""id"": ""455c15d2-2e30-4fbe-8bb9-f41fe5782bd7"",
-                    ""path"": ""1DAxis"",
+                    ""name"": ""2D Vector"",
+                    ""id"": ""9492171c-7b03-48ab-89bd-4039dbd4da28"",
+                    ""path"": ""2DVector"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -40,8 +40,30 @@ public class @Inputmaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 },
                 {
-                    ""name"": ""negative"",
-                    ""id"": ""aaa09b61-3d38-4086-b525-cf1f73477c92"",
+                    ""name"": ""up"",
+                    ""id"": ""7c4f0609-e9ba-46ce-acc9-312795857eab"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""2a996df8-2b8c-447e-b6a2-a1c71a946573"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movment"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""c588c57f-55ff-46bb-813a-1bcacc13f896"",
                     ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -51,8 +73,8 @@ public class @Inputmaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""positive"",
-                    ""id"": ""fa73c914-a775-4399-ace3-606c7d161e3b"",
+                    ""name"": ""right"",
+                    ""id"": ""0f2964d2-319f-4572-b3af-d91b5207c1c2"",
                     ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -60,55 +82,6 @@ public class @Inputmaster : IInputActionCollection, IDisposable
                     ""action"": ""Movment"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""negative"",
-                    ""id"": ""f974755b-7146-4c5f-b0eb-0a875c638946"",
-                    ""path"": ""<Keyboard>/leftArrow"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movment"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""positive"",
-                    ""id"": ""e3dec4f1-3866-4ed6-b11b-7cb87581f9ad"",
-                    ""path"": ""<Keyboard>/rightArrow"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movment"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                }
-            ]
-        },
-        {
-            ""name"": ""Debug"",
-            ""id"": ""992de2ce-7910-446e-8d1e-aa0e297ab38e"",
-            ""actions"": [
-                {
-                    ""name"": ""ButtonPressA"",
-                    ""type"": ""Button"",
-                    ""id"": ""09317a6f-4c45-404e-91ac-fb08e297cdcf"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """"
-                }
-            ],
-            ""bindings"": [
-                {
-                    ""name"": """",
-                    ""id"": ""2d4a13a1-2e6d-47a4-a36d-136f847e5b86"",
-                    ""path"": ""<Keyboard>/a"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""ButtonPressA"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -118,9 +91,6 @@ public class @Inputmaster : IInputActionCollection, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Movment = m_Player.FindAction("Movment", throwIfNotFound: true);
-        // Debug
-        m_Debug = asset.FindActionMap("Debug", throwIfNotFound: true);
-        m_Debug_ButtonPressA = m_Debug.FindAction("ButtonPressA", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -199,45 +169,8 @@ public class @Inputmaster : IInputActionCollection, IDisposable
         }
     }
     public PlayerActions @Player => new PlayerActions(this);
-
-    // Debug
-    private readonly InputActionMap m_Debug;
-    private IDebugActions m_DebugActionsCallbackInterface;
-    private readonly InputAction m_Debug_ButtonPressA;
-    public struct DebugActions
-    {
-        private @Inputmaster m_Wrapper;
-        public DebugActions(@Inputmaster wrapper) { m_Wrapper = wrapper; }
-        public InputAction @ButtonPressA => m_Wrapper.m_Debug_ButtonPressA;
-        public InputActionMap Get() { return m_Wrapper.m_Debug; }
-        public void Enable() { Get().Enable(); }
-        public void Disable() { Get().Disable(); }
-        public bool enabled => Get().enabled;
-        public static implicit operator InputActionMap(DebugActions set) { return set.Get(); }
-        public void SetCallbacks(IDebugActions instance)
-        {
-            if (m_Wrapper.m_DebugActionsCallbackInterface != null)
-            {
-                @ButtonPressA.started -= m_Wrapper.m_DebugActionsCallbackInterface.OnButtonPressA;
-                @ButtonPressA.performed -= m_Wrapper.m_DebugActionsCallbackInterface.OnButtonPressA;
-                @ButtonPressA.canceled -= m_Wrapper.m_DebugActionsCallbackInterface.OnButtonPressA;
-            }
-            m_Wrapper.m_DebugActionsCallbackInterface = instance;
-            if (instance != null)
-            {
-                @ButtonPressA.started += instance.OnButtonPressA;
-                @ButtonPressA.performed += instance.OnButtonPressA;
-                @ButtonPressA.canceled += instance.OnButtonPressA;
-            }
-        }
-    }
-    public DebugActions @Debug => new DebugActions(this);
     public interface IPlayerActions
     {
         void OnMovment(InputAction.CallbackContext context);
-    }
-    public interface IDebugActions
-    {
-        void OnButtonPressA(InputAction.CallbackContext context);
     }
 }
