3a210ef560fc63a3347ad3e2068671f1d2633956
SjoerdDeKoning <78423327+SjoerdDeKoning@users.noreply.github.com>
2021-02-03 10:37:59 +0100

    remove inputmaser cs

--- Assets/Dev/Sjoerd/scripts/PlayerController.cs
+++ Assets/Dev/Sjoerd/scripts/PlayerController.cs
@@ -10,13 +10,11 @@ namespace Sjoerd
         [SerializeField] float speed;
         [SerializeField] int maxDistance;
 
-        public Inputmaster controls;
         [SerializeField] private Vector3 adToMovment;
         private Vector3 playerStartingPos;
 
         private void Awake()
         {
-            controls = new Inputmaster();
             playerStartingPos = transform.position;
         }      
         public void MovmentInputInfo(InputAction.CallbackContext ctx )
@@ -35,14 +33,6 @@ namespace Sjoerd
             if (transform.position.x > playerStartingPos.x - maxDistance && adToMovment.x <= 0)
                 transform.position = transform.position + adToMovment;
         }       
-        private void OnEnable()
-        {
-            controls.Enable();
-        }
-        private void OnDisable()
-        {
-            controls.Disable();
-        }
     }
 }
 
