01f7491fbb9d8483dae37855757adca218172765
SjoerdDeKoning <78423327+SjoerdDeKoning@users.noreply.github.com>
2021-02-03 10:56:41 +0100

    player controlller updact

--- Assets/Dev/Sjoerd/scripts/PlayerController.cs
+++ Assets/Dev/Sjoerd/scripts/PlayerController.cs
@@ -10,29 +10,33 @@ namespace Sjoerd
         [SerializeField] float speed;
         [SerializeField] int maxDistance;
 
-        [SerializeField] private Vector3 adToMovment;
+        [SerializeField] private Vector2 movmentInfo;
         private Vector3 playerStartingPos;
+        private Rigidbody2D rb;
+        private BoxCollider2D box;
+        [SerializeField] private Vector2 baseMovelimit;
 
         private void Awake()
         {
             playerStartingPos = transform.position;
+            rb = GetComponent<Rigidbody2D>();
+            box = GetComponent<BoxCollider2D>();
         }      
         public void MovmentInputInfo(InputAction.CallbackContext ctx )
         {
-            adToMovment = ctx.ReadValue<Vector2>() * speed * Time.deltaTime;
-        }
-        private void Update()
-        {
-            Movment();
+            movmentInfo = ctx.ReadValue<Vector2>();
         }
         private void Movment()
         {
-            if (transform.position.x < playerStartingPos.x + maxDistance && adToMovment.x >= 0)
-                transform.position = transform.position + adToMovment;
-
-            if (transform.position.x > playerStartingPos.x - maxDistance && adToMovment.x <= 0)
-                transform.position = transform.position + adToMovment;
-        }       
+            Vector2 moveLimet = baseMovelimit - (Vector2)box.bounds.size * 0.5f;
+            Vector2 newPos = rb.position + (movmentInfo * speed * Time.fixedDeltaTime);
+            newPos.x = Mathf.Clamp(newPos.x, -moveLimet.x, moveLimet.x);
+            rb.MovePosition(newPos);
+        }
+        private void FixedUpdate()
+        {
+            Movment();
+        }
     }
 }
 
